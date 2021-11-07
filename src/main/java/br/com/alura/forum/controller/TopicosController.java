ce0937849b06873258d6a45857d33981e96a0197
Henrique Naimaier <coubron@yahoo.com.br>
2021-11-07 09:30:25 -0300

    First commit

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,109 @@
+package br.com.alura.forum.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDTO;
+import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	@Autowired
+	private CursoRepository cursoRepository;
+
+	@GetMapping
+	public List<TopicoDTO> lista(String nomeCurso) {
+		List<Topico> topicos;
+		
+		if (nomeCurso == null) {
+			topicos = topicoRepository.findAll();
+		} else {
+			topicos = topicoRepository.findByCursoNome(nomeCurso);
+		}
+		
+		
+		return TopicoDTO.converter(topicos);
+	}
+	
+	@PostMapping
+	@Transactional
+	private ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+	}
+	
+	@GetMapping("/{id}")
+	private ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id) {
+		
+		Optional<Topico> topico = topicoRepository.findById(id);
+		
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDTO(topico.get()));			
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	// Para commitar ao final do método
+	private ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		
+		Optional<Topico> optional = topicoRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDTO(topico));						
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	private ResponseEntity<?> remover(@PathVariable Long id) {
+		// Usamos a ? no lugar do generics pra dizer que a gente não sabe o tipo, só pra ele não reclamar
+		
+		Optional<Topico> topico = topicoRepository.findById(id);
+		
+		if (topico.isPresent()) {
+			topicoRepository.deleteById(id);
+			
+			return ResponseEntity.ok().build();
+			// Para construir a resposta sem corpo
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+}
