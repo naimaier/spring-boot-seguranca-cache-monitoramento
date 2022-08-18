ecbacf78353d9609370816e4195ad959ff784893
Henrique Naimaier <coubron@yahoo.com.br>
2022-08-18 10:28:57 -0300

    Added actuator and configured project for use with Spring Boot Admin

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -48,6 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated()
 			.and().csrf().disable()
